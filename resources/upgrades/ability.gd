b6129017a90ec9400228ba0480e8b62d9dfc26e3
Dennis <309233594@qq.com>
2024-03-29 15:46:50 +0800

    增加斧头技能和斧头升级

---
+++ resources/upgrades/ability.gd
@@ -0,0 +1,5 @@
+extends AbilityUpgrade
+
+class_name Ability
+
+@export var ability_controller_scene:PackedScene
--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -6,6 +6,7 @@ const ACCELERATION_SMOOTHING = 25
 @onready var damage_interval_timer = $DamageIntervalTimer
 @onready var health_component = $HealthComponent
 @onready var health_bar = $ProgressBar
+@onready var abilities = $Abilities
 
 var number_colliding_bodies = 0
 
@@ -14,6 +15,7 @@ func _ready():
 	$CollisionArea2D.body_exited.connect(on_body_exited)
 	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
 	health_component.health_changed.connect(on_health_changed)
+	GameEvents.ability_upgrade_added.connect(on_ability_upgrade_added)
 	update_health_display()
 
 func _process(delta):
@@ -50,3 +52,10 @@ func on_damage_interval_timer_timeout():
 
 func on_health_changed():
 	update_health_display()
+	
+func on_ability_upgrade_added(ability_upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	if not ability_upgrade is Ability:
+		return
+
+	var ability = ability_upgrade as Ability
+	abilities.add_child(ability.ability_controller_scene.instantiate())
