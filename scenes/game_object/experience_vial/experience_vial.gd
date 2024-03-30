3fd36315b82dc778880dd9cba8c2fa6cb7dc8d3e
Dennis <309233594@qq.com>
2024-03-30 14:31:51 +0800

    增加窗口主题字体
    P43 14. Adding a Custom Font

--- scenes/game_object/experience_vial/experience_vial.gd
+++ scenes/game_object/experience_vial/experience_vial.gd
@@ -21,7 +21,7 @@ func tween_collect(percent: float, start_position:Vector2):
 	rotation = 	lerp_angle(rotation, target_rotation, 1 - exp(2 * -get_process_delta_time()))
 
 func collect():
-	#GameEvents.emit_experience_vial_collected(1)
+	GameEvents.emit_experience_vial_collected(1)
 	queue_free()
 
 
