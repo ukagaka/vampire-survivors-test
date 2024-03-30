360832527f2169b701c5936afbd3c8bb9ec4f934
Dennis <309233594@qq.com>
2024-03-30 19:04:19 +0800

    P48 19. 完善升级技能 UI 面板字体
    P48 19. Finalizing Upgrade Card Theme

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -2,13 +2,15 @@ extends PanelContainer
 
 signal selected
 
-@onready var name_label: Label = $VBoxContainer/NameLabel
-@onready var description_label: Label = $VBoxContainer/DescriptionLabel
+@onready var name_label = %NameLabel
+@onready var description_label: Label = %DescriptionLabel
+
 
 func _ready():
 	gui_input.connect(on_gui_input)
 
 func set_ability_upgrade(upgrade: AbilityUpgrade):
+	print(upgrade.name)
 	name_label.text = upgrade.name
 	description_label.text = upgrade.description
 	
