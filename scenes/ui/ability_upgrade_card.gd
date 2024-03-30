5c36cd0ae126b334927caf2ae3c0a3d1f086724f
Dennis <309233594@qq.com>
2024-03-30 19:50:55 +0800

    P49 20. 升级选项卡选择与退出UI
    P49 20. Animating the Upgrade Card

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -5,15 +5,46 @@ signal selected
 @onready var name_label = %NameLabel
 @onready var description_label: Label = %DescriptionLabel
 
+#记录一个变量，防止用户连点，重复执行
+var disabled = false
 
 func _ready():
 	gui_input.connect(on_gui_input)
+	mouse_entered.connect(on_mouse_entered)
+	
+func play_in(delay: float = 0):
+	modulate = Color.TRANSPARENT
+	await get_tree().create_timer(delay).timeout
+	$AnimationPlayer.play("in")
+	
+func play_discard():
+	$AnimationPlayer.play("discard")
 
 func set_ability_upgrade(upgrade: AbilityUpgrade):
 	print(upgrade.name)
 	name_label.text = upgrade.name
 	description_label.text = upgrade.description
 	
+func select_card():
+	disabled = true
+	$AnimationPlayer.play("selected")
+	
+	for other_card in get_tree().get_nodes_in_group("upgrade_card"):
+		if other_card == self:
+			continue
+		other_card.play_discard()
+	
+	await $AnimationPlayer.animation_finished
+	selected.emit()
+	
 func on_gui_input(event:InputEvent):
+	if disabled:
+		return
+		
 	if event.is_action_pressed("left_click"):
-		selected.emit()
+		select_card()
+		
+func on_mouse_entered():
+	if disabled:
+		return
+	$HoverAnimationPlayer.play("hover")
--- scenes/ui/upgrade_screen.gd
+++ scenes/ui/upgrade_screen.gd
@@ -9,11 +9,14 @@ func _ready():
 	get_tree().paused = true
 
 func set_ability_upgrades(upgrades: Array[AbilityUpgrade]):
+	var delay = 0
 	for upgrade in upgrades:
 		var card_instance = upgrade_card_scene.instantiate()
 		card_container.add_child(card_instance)
 		card_instance.set_ability_upgrade(upgrade)
+		card_instance.play_in(delay)
 		card_instance.selected.connect(on_upgrade_selected.bind(upgrade))
+		delay += 0.2
 
 func on_upgrade_selected(upgrade: AbilityUpgrade):
 	upgrade_selected.emit(upgrade)
