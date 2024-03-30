34571cca485af752d2fb0ce952adf750fc1ffe34
Dennis <309233594@qq.com>
2024-03-30 10:51:14 +0800

    设置巫师动画
    P41 12. Animating the Wizard

--- scenes/component/velocity_component.gd
+++ scenes/component/velocity_component.gd
@@ -23,6 +23,10 @@ func accelerate_in_direction(direction: Vector2):
 	var desired_velocity = direction * max_speed
 	velocity = velocity.lerp(desired_velocity, 1 - exp(-acceleration * get_process_delta_time()))
 
+func decelerate():
+	accelerate_in_direction(Vector2.ZERO)
+
+
 func move(character_body: CharacterBody2D):
 	character_body.velocity = velocity
 	character_body.move_and_slide()
--- scenes/game_object/wizard_enemy/wizard_enemy.gd
+++ scenes/game_object/wizard_enemy/wizard_enemy.gd
@@ -3,12 +3,20 @@ extends CharacterBody2D
 @onready var velocity_component = $VelocityComponent
 @onready var visuals = $Visuals
 
+var is_moving = false
 
 func _process(delta):
 	
-	velocity_component.accelerate_to_player()
+	if is_moving:
+		velocity_component.accelerate_to_player()
+	else: 
+		velocity_component.decelerate()
+		
 	velocity_component.move(self)
 	
 	var move_sign = sign(velocity.x)
 	if move_sign != 0:
 		visuals.scale = Vector2(move_sign, 1)
+		
+func set_is_moving(moving: bool):
+	is_moving = moving
