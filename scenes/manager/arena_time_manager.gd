225a846ec12a5d8edcd4d2288ff84927a225f161
Dennis <309233594@qq.com>
2024-03-31 17:05:00 +0800

    P60 5. 添加胜利和结束时的音效
    P60 5. Adding Victory and Defeat Jingles

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -25,4 +25,4 @@ func _process(delta):
 func on_timer_timeout():
 	var end_screen_instance = end_screen_scene.instantiate()
 	add_child(end_screen_instance)
-	end_screen_instance.set_defeat()
+	end_screen_instance.play_jingle()
--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -18,6 +18,13 @@ func _ready():
 func set_defeat():
 	$%TitleLabel.text = "Defeat"
 	$%DescriptionLabel.text = "You lost!"
+	play_jingle(true)
+	
+func play_jingle(defeat: bool = false):
+	if defeat:
+		$DefeatStreamPlayer.play()
+	else:
+		$VictoryStreamPlayer.play()
 
 func on_restart_button_pressed():
 	get_tree().paused = false
