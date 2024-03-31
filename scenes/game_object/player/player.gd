44df1dc2894282a9a195b1deea2f4209e55768f6
Dennis <309233594@qq.com>
2024-03-31 11:50:07 +0800

    P54 25. 增加角色移动速度的升级技能
    P54 25. Creating a Player Move Speed Upgrade

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -1,7 +1,5 @@
 extends CharacterBody2D
 
-const MAX_SPEED = 125
-const ACCELERATION_SMOOTHING = 25
 
 @onready var damage_interval_timer = $DamageIntervalTimer
 @onready var health_component = $HealthComponent
@@ -9,10 +7,14 @@ const ACCELERATION_SMOOTHING = 25
 @onready var abilities = $Abilities
 @onready var animation_player = $AnimationPlayer
 @onready var visuals = $Visuals
+@onready var velocity_component = $VelocityComponent
+
 
 var number_colliding_bodies = 0
+var base_speed = 0
 
 func _ready():
+	base_speed = velocity_component.max_speed
 	$CollisionArea2D.body_entered.connect(on_body_entered)
 	$CollisionArea2D.body_exited.connect(on_body_exited)
 	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
@@ -23,10 +25,8 @@ func _ready():
 func _process(delta):
 	var movenent_vector = get_movement_vector()
 	var direction = movenent_vector.normalized()
-	var target_velocity = direction * MAX_SPEED
-	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
-	
-	move_and_slide()
+	velocity_component.accelerate_in_direction(direction)
+	velocity_component.move(self)
 	
 	if movenent_vector.x != 0 || movenent_vector.y != 0 :
 		animation_player.play("wail")
@@ -66,8 +66,9 @@ func on_health_changed():
 	update_health_display()
 	
 func on_ability_upgrade_added(ability_upgrade: AbilityUpgrade, current_upgrades: Dictionary):
-	if not ability_upgrade is Ability:
-		return
+	if ability_upgrade is Ability:
+		var ability = ability_upgrade as Ability
+		abilities.add_child(ability.ability_controller_scene.instantiate())
+	elif ability_upgrade.id == "player_speed":
+		velocity_component.max_speed = base_speed + (base_speed * current_upgrades["player_speed"]["quantity"]* 1)
 
-	var ability = ability_upgrade as Ability
-	abilities.add_child(ability.ability_controller_scene.instantiate())
--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -10,11 +10,13 @@ var upgrade_axe = preload("res://resources/upgrades/axe.tres")
 var upgrade_axe_damage = preload("res://resources/upgrades/axe_damage.tres")
 var upgrade_sword_rate = preload("res://resources/upgrades/sword_rate.tres")
 var upgrade_sword_damage = preload("res://resources/upgrades/sword_damage.tres")
+var player_speed = preload("res://resources/upgrades/player_speed.tres")
 
 func _ready():
 	upgrade_pool.add_item(upgrade_axe, 10)
-	upgrade_pool.add_item(upgrade_sword_rate, 10)
-	upgrade_pool.add_item(upgrade_sword_damage, 10)
+	#upgrade_pool.add_item(upgrade_sword_rate, 10)
+	#upgrade_pool.add_item(upgrade_sword_damage, 10)
+	upgrade_pool.add_item(player_speed, 5)
 	
 	experience_manager.level_up.connect(on_level_up)
 	
