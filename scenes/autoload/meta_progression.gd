7a04517cac78d1fe8ffe12cf6a60ef830cf3369b
Dennis <309233594@qq.com>
2024-04-02 13:30:57 +0800

    P68 7. 保存用户信息
    P68 7. Saving and Loading Meta Progression Data

--- scenes/autoload/meta_progression.gd
+++ scenes/autoload/meta_progression.gd
@@ -1,5 +1,6 @@
 extends Node
 
+const SAVE_FILE_PATH = "user://game.save"
 
 var save_data: Dictionary = {
 	"meta_upgrade_currency": 0,
@@ -9,8 +10,20 @@ var save_data: Dictionary = {
 
 func _ready():
 	GameEvents.experience_vial_collected.connect(on_experience_collected)
-	add_meta_upgrade(load("res://resources/meta_upgrades/experience_gain.tres"))
+	load_save_file()
 
+
+func load_save_file():
+	if !FileAccess.file_exists(SAVE_FILE_PATH):
+		return
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
+	save_data = file.get_var()
+	
+
+func save():
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
+	file.store_var(save_data)
+	
 
 func add_meta_upgrade(upgrade: MetaUpgrade):
 	if !save_data["meta_upgrades"].has(upgrade.id):
@@ -19,7 +32,13 @@ func add_meta_upgrade(upgrade: MetaUpgrade):
 		}
 	
 	save_data["meta_upgrades"][upgrade.id]["quantity"] += 1
-	print(save_data)
+	save()
+	
+	
+func get_upgrade_count(upgrade_id: String):
+	if save_data["meta_upgrades"].has(upgrade_id):
+		return save_data["meta_upgrades"][upgrade_id]["quantity"]
+	return 0
 	
 
 func on_experience_collected(number: float):
